dfd81f715fe5e0b02a9c66fa2b9dd5a61e60e552
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2024-12-03 09:01:30 -0500

    first commit

--- Grademanagement/Student.py
+++ Grademanagement/Student.py
@@ -1,11 +1,8 @@
+import csv
 
 class Student: 
-    report = {
-        'name': {
-            'Subject': {
-                'marks': ''
-            }
-        }
+    reports = {
+       
     }
     def __init__(self, name, roll_number, subject, marks, teacher):
         self.name = name 
@@ -15,7 +12,7 @@ class Student:
         self.marks = marks
 
     def add_marks(self): 
-        Student.report[self.name] = {
+        Student.reports[self.name] = {
             self.subject: {
                 'Enroll Number': self.roll_number,
                 'marks': self.marks,
@@ -29,21 +26,41 @@ class Student:
         pass 
 
     def show_report(self): 
-        # for key, value in Student.report.items(): 
-        #         print(f'here is the report for {self.name}, {key}, {value}')
-        print(Student.report)
-
+        with open('results.csv', mode='w', encoding='utf-8', newline='') as file: 
+                    writer = csv.writer(file)
+                   
+                    writer.writerow(['Student Name', 'Subject', 'Enroll Number', 'Marks', 'Teacher'])
+                    for student, marks in Student.reports.items(): 
+                        print(f'student: {student}')
+                        for subject, details in marks.items(): 
+                            print(f'subject: {subject}')            
+                            print(f"Enrollment Number: {details['Enroll Number']}")
+                            print(f"Marks: {details['marks']}")
+                            print(f"Teacher: {details['teacher']}\n")
+                            writer.writerow([student, subject, details['Enroll Number'], details['marks'], details['teacher']])
+        
+        print('successfully saved')
 
 
 student1 = Student('James Shuta', 454453, 'physics', 60/100, 'Isaac')
 student2 = Student('Nkenganyi Clarence',344333,  'Biology', 95, 'Philip')
 student3 = Student('Jingwa Laura',344333,  'Geography', 95, 'Philip')
+student4 = Student('Anyichap Sandra', 3944333,  'Programming 101', 65, 'Lechendem Stephen')
+student5 = Student('Anyichap Sandra', 3944333,  'Frontend Development ', 65, 'Lechendem Stephen')
+
+
+
 
 student3.add_marks()
 student1.add_marks()
 student2.add_marks()
+student4.add_marks()
+student5.add_marks()
+
 
 student2.show_report()
+
+
 
 
 
